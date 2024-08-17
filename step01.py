2d4428416561b4d827186295aff9f10a7167e33f
cedric <song921216@gmail.com>
2024-08-17 12:30:31 +0900

    avoid miss calling

--- step01.py
+++ step01.py
@@ -2,8 +2,10 @@ class Variable:
     def __init__(self, data) -> None:
         self.data = data
 
-import numpy as np
 
-data = np.array(2.0)
-x = Variable(data)
-print(x.data)
+if __name__ == "__main__":
+    import numpy as np
+
+    data = np.array(2.0)
+    x = Variable(data)
+    print(x.data)
