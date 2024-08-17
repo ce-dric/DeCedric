2821d8fa9c5333360971f31224ba5f472dcb614a
cedric <song921216@gmail.com>
2024-08-17 17:55:38 +0900

    compositional function

--- step04.py
+++ step04.py
@@ -1,6 +1,7 @@
 import numpy as np
 from step01 import Variable
 from step02 import Square
+from step03 import Exp
 
 def numerical_diff(f, x, eps=1e-4):
     x0 = Variable(x.data - eps)
@@ -9,8 +10,14 @@ def numerical_diff(f, x, eps=1e-4):
     y1 = f(x1)
     return (y1.data - y0.data) / (2 * eps)
 
+def f(x):
+    A = Square()
+    B = Exp()
+    C = Square()
+    return C(B(A(x)))
+
 if __name__ == "__main__":
-    f = Square()
-    x = Variable(np.array(2.0))
+    x = Variable(np.array(0.5))
     dy = numerical_diff(f, x)
     print(dy)
+    
