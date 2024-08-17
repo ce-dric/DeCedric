22e4a3034ea2ca69de51282dd825840d39703a64
cedric <song921216@gmail.com>
2024-08-17 18:50:50 +0900

    for usage convenience

---
+++ step09.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+from step08 import Square, Exp, Variable
+
+def square(x):
+    f = Square()
+    return f(x)
+
+def exp(x):
+    f = Exp()
+    return f(x)
+
+if __name__ == "__main__":
+    x = Variable(np.array(0.5))
+    a = square(x)
+    b = exp(a)
+    y = square(b)
+
+    # backward
+    y.grad = np.array(1.0)
+    y.backward()
+    print(x.grad)
