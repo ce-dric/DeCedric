e6b41f74ee785263ee3e8a15acefb223947a117a
cedric <song921216@gmail.com>
2024-08-18 11:27:43 +0900

    step19_overwriting

--- main.py
+++ main.py
@@ -18,14 +18,41 @@ class Config:
     enable_backprop = True
 
 class Variable:
-    def __init__(self, data):
+    def __init__(self, data, name=None):
         if data is not None:
             if not isinstance(data, np.ndarray):
                 raise TypeError('{} does not supported'.format(type(data)))
+            
         self.data = data
+        self.name = name
         self.grad = None
         self.creator = None
         self.generation = 0
+
+    def __len__(self):
+        return len(self.data)
+    
+    def __repr__(self):
+        if self.data is None:
+            return 'variable(None)'
+        p = str(self.data).replace('\n', '\n' + ' ' * 9)
+        return 'variable(' + p + ')'
+    
+    @property
+    def shape(self):
+        return self.data.shape
+    
+    @property
+    def ndim(self):
+        return self.data.ndim
+    
+    @property
+    def size(self):
+        return self.data.size
+    
+    @property
+    def dtype(self):
+        return self.data.dtype
     
     def set_creator(self, func):
         self.creator = func
@@ -133,3 +160,6 @@ if __name__ == "__main__":
 
     print(y.grad, t.grad)
     print(x0.grad, x1.grad)
+
+    x2 = Variable(np.array([[1, 2, 3], [4, 5, 6]]))
+    print(x2)
