a150f7fc757e30a513898d96c5ef49b1176d13ff
cedric <song921216@gmail.com>
2024-08-18 09:45:09 +0900

    for devel convenience

--- step12.py
+++ step12.py
@@ -33,12 +33,16 @@ def as_array(x):
 class Function:
     def __call__(self, *inputs):
         xs = [x.data for x in inputs]
-        ys = self.forward(xs)
+        ys = self.forward(*xs)
+        if not isinstance(ys, tuple):
+            ys = (ys,)
         outputs = [Variable(as_array(y)) for y in ys]
+        
         for output in outputs:
             output.set_creator(self)
         self.inputs = inputs
         self.outputs = outputs
+        
         return outputs if len(outputs) > 1 else outputs[0]
     
     def forward(self, xs):
@@ -48,14 +52,15 @@ class Function:
         raise NotImplementedError()
     
 class Add(Function):
-    def forward(self, xs):
-        x0, x1 = xs
+    def forward(self, x0, x1):
         y = x0 + x1
-        return (y,)
+        return y
+    
+def add(x0, x1):
+    return Add()(x0, x1)
     
 if __name__ == "__main__":
     x0 = Variable(np.array(2))
     x1 = Variable(np.array(3))
-    f = Add()
-    y = f(x0, x1)
+    y = add(x0, x1)
     print(y.data)
