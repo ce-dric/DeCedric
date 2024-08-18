3f25dc663cae842e3c86f6fa19fe6274570a5216
cedric <song921216@gmail.com>
2024-08-18 17:36:15 +0900

    neg, sub, div, pow Impl

--- main.py
+++ main.py
@@ -178,7 +178,80 @@ def as_variable(obj):
         return obj
     return Variable(obj)
 
+class Neg(Function):
+    def forward(self, x):
+        return -x
+    
+    def backward(self, gy):
+        return -gy
+
+def neg(x):
+    return Neg()(x)
+
+Variable.__neg__ = neg
+
+class Sub(Function):
+    def forward(self, x0, x1):
+        y = x0-x1
+        return y
+    
+    def backward(self, gy):
+        return gy, -gy
+    
+def sub(x0, x1):
+    x1 = as_array(x1)
+    return Sub()(x0, x1)
+
+def rsub(x0, x1):
+    x1 = as_array(x1)
+    return Sub()(x1, x0)
+
+Variable.__sub__ = sub
+Variable.__rsub__ = rsub
+
+class Div(Function):
+    def forward(self, x0, x1):
+        y = x0/x1
+        return y
+    
+    def backward(self, gy):
+        x0, x1 = self.inputs[0].data, self.inputs[1].data
+        gx0 = gy / x1
+        gx1 = gy * (-x0/x1**2)
+        return gx0, gx1
+
+def div(x0, x1):
+    x1 = as_array(x1)
+    return Div()(x0, x1)
+
+def rdiv(x0, x1):
+    x1 = as_array(x1)
+    return Div()(x1, x0)
+
+Variable.__truediv__ = div
+Variable.__rturediv__ = rdiv
+
+class Pow(Function):
+    def __init__(self, c):
+        self.c = c
+    
+    def forward(self, x):
+        y = x ** self.c
+        return y
+    
+    def backward(self, gy):
+        x = self.inputs[0].data
+        c = self.c
+        gx = c * x ** (c - 1) * gy
+        return gx
+
+def pow(x, c):
+    return Pow(c)(x)
+
+Variable.__pow__ = pow
+    
+
 if __name__ == "__main__":
-    x = Variable(np.array(1.0))
-    y = np.array([2.0]) + x
+    x = Variable(np.array(2.0))
+    y = x ** 3
     print(y)
