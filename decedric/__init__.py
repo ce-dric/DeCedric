1d21be0b917f055f3a86e997a6cbc8ab5024faad
cedric <song921216@gmail.com>
2024-08-24 12:38:15 +0900

    need to findout where is wrong

--- decedric/__init__.py
+++ decedric/__init__.py
@@ -1,10 +1,21 @@
-from decedric.core_simple import Variable
-from decedric.core_simple import Function
-from decedric.core_simple import using_config
-from decedric.core_simple import no_grad
-from decedric.core_simple import as_array
-from decedric.core_simple import as_variable
-from decedric.core_simple import setup_variable
-from decedric.core_simple import sin
+is_simple_core = False
 
+if is_simple_core:
+    from decedric.core_simple import Variable
+    from decedric.core_simple import Function
+    from decedric.core_simple import using_config
+    from decedric.core_simple import no_grad
+    from decedric.core_simple import as_array
+    from decedric.core_simple import as_variable
+    from decedric.core_simple import setup_variable
+
+else:
+    from decedric.core import Variable
+    from decedric.core import Function
+    from decedric.core import using_config
+    from decedric.core import no_grad
+    from decedric.core import as_array
+    from decedric.core import as_variable
+    from decedric.core import setup_variable
+    
 setup_variable()
--- main.py
+++ main.py
@@ -3,28 +3,19 @@ if '__file__' in globals():
     sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 
 import numpy as np
-from decedric import Variable, sin
-from decedric.utils import plot_dot_graph
+from decedric import Variable
 
-def sphere(x, y):
-    z = x ** 2 + y ** 2
-    return z
-
-def matyas(x, y):
-    z = 0.26 * (x ** 2 + y ** 2) - 0.48 * x * y
-    return z
-
-def goldstein(x, y):
-    z = (1+(x+y+1)**2*(19-14*x+3*x**2-14*y+6*x*y+3*y**2))*(30+(2*x-3*y)**2*(18-32*x+12*x**2+48*y-36*x*y+27*y**2))
-    return z
-
-def rosenbrock(x0, x1):
-    y = 100 * (x1 - x0**2) **2 + (1-x0)**2
+def f(x):
+    y = x ** 4 - 2 * x ** 2
     return y
 
-x0 = Variable(np.array(0.0))
-x1 = Variable(np.array(2.0))
-y = rosenbrock(x0, x1)
-y.backward()
+x = Variable(np.array(2.0))
+x.cleargrad()
+y = f(x)
+y.backward(create_graph=True)
+print(x.grad)
 
-print(x0.grad, x1.grad)
+gx = x.grad
+x.cleargrad()
+gx.backward()
+print(x.grad)
