21f28339ee1e6fe82637ff2467473302aa6aee3c
cedric <song921216@gmail.com>
2024-08-25 11:51:23 +0900

    tested work with reshape

--- main.py
+++ main.py
@@ -7,6 +7,6 @@ import decedric.functions as F
 from decedric import Variable
 
 x = Variable(np.array([[1,2,3],[4,5,6]]))
-c = Variable(np.array([[10,20,30],[40,50,60]]))
-y = x+c
-print(y)
+y = F.reshape(x, (6,))
+y.backward(retain_grad=True)
+print(x.grad)
