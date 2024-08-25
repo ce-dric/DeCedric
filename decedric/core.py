b7e83ab7ca48fb484e761aababea12acef9dd524
cedric <song921216@gmail.com>
2024-08-25 11:44:05 +0900

    move sin to functions

--- decedric/core.py
+++ decedric/core.py
@@ -226,19 +226,6 @@ class Pow(Function):
 
 def pow(x, c):
     return Pow(c)(x)
-
-class Sin(Function):
-    def forward(self, x):
-        y = np.sin(x)
-        return y
-    
-    def backward(self, gy):
-        x = self.inputs[0].data
-        gx = gy * np.cos(x)
-        return gx
-
-def sin(x):
-    return Sin()(x)
     
 @contextlib.contextmanager
 def using_config(name, value):
--- main.py
+++ main.py
@@ -3,18 +3,10 @@ if '__file__' in globals():
     sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 
 import numpy as np
+import decedric.functions as F
 from decedric import Variable
 
-def f(x):
-    y = x ** 4 - 2 * x ** 2
-    return y
-
-x = Variable(np.array(2.0))
-y = f(x)
-y.backward(create_graph=True)
-print(x.grad)
-
-gx = x.grad
-x.cleargrad()
-gx.backward()
-print(x.grad)
+x = Variable(np.array([[1,2,3],[4,5,6]]))
+c = Variable(np.array([[10,20,30],[40,50,60]]))
+y = x+c
+print(y)
